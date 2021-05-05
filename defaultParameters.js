e00436e0fb0e7bdb2219d04fec3cb3f648f69903
Ayushman09 <ayushmanchak@gmail.com>
2021-05-05 11:02:01 +0530

    Default Param Assignment

---
+++ defaultParameters.js
@@ -0,0 +1,9 @@
+function greetStudent(name,message = "Welcome!")
+{
+    console.log("Hi " + name + ", " + message);
+}
+
+greetStudent('Ayushman', 'Congratulations!');
+greetStudent('Ravi');
+greetStudent('Satyam', 'How are you?');
+greetStudent('Manas!');
