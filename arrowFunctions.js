7c0dfcb464336911b9afa3423edaf70538b4d49c
Ayushman09 <ayushmanchak@gmail.com>
2021-05-05 10:53:01 +0530

    ArrowFunctions Assignment

---
+++ arrowFunctions.js
@@ -0,0 +1,8 @@
+const nameOfStudents = ['Abhijit', 'Ayushman', 'Barun','Dipanwita','Fahim','Ravi'];
+const heightsOfStudents = [170,179,176,156,160,181];
+const ageOfStudents = [21,20,22,22,21,20];
+
+//using arrow functions
+const studentsDetails = nameOfStudents.map((studentName,index) => ({Name : studentName, Age : ageOfStudents[index], heightInCM: heightsOfStudents[index]}));
+
+console.log(studentsDetails)
